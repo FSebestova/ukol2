64600d80d43dfc037c9297ede537749ef80d1a81
Františka Šebestová <fsebestova@seznam.cz>
2024-11-24 16:02:45 +0100

    ukol2

--- ukol2.js
+++ ukol2.js
@@ -1,15 +1,15 @@
 const justFood = (clovek) => {
-    const cena = clovek * 100;  // 100 Kč na osobu
+    const cena = clovek * 150;  // 100 Kč na osobu
     return `Catering od Just Food pro ${clovek} lidí za ${cena} Kč`;
 };
 
 const yourMama = (clovek) => {
-    const cena = clovek * 500;  // 500 Kč na osobu
+    const cena = clovek * 300;  // 500 Kč na osobu
     return `Catering od Your Mama pro ${clovek} lidí za ${cena} Kč`;
 };
 
 const flavourHaven = (clovek) => {
-    const cena = clovek * 3000;  // 3000 Kč na osobu
+    const cena = clovek * 2000;  // 3000 Kč na osobu
     return `Catering od Flavour Haven pro ${clovek} lidí za ${cena} Kč`;
 };
 
@@ -19,6 +19,6 @@ const vytvoreniUdalosti = (jmenoUdalosti, clovek, cateringFunkce) => {
 };
 
 console.log(justFood(100));
-console.log(yourMama(100));
-console.log(flavourHaven(100));
-console.log(vytvoreniUdalosti('Vánoční večírek', 100, flavourHaven));
+console.log(yourMama(30));
+console.log(flavourHaven(120));
+console.log(vytvoreniUdalosti('Vánoční večírek', 300, flavourHaven));
